bbfa3fa9b068e112b1b9b8d623dbd0b42c122cce
mmenafra <mmenafra@gmail.com>
2017-11-07 11:44:48 -0300

    chg: added gzip json

--- nginx/attributes/nginx.rb
+++ nginx/attributes/nginx.rb
@@ -54,7 +54,8 @@ default[:nginx][:gzip_types] = ["application/x-javascript",
                                 "text/css",
                                 "text/javascript",
                                 "text/plain",
-                                "text/xml"]
+                                "text/xml",
+                                "application/json"]
 # NGinx will compress "text/html" by default
 
 default[:nginx][:keepalive] = "on"
