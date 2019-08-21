2ca3b7ad767a65ac2e60544c86ccab7d89d28a35
Matias Menafra <mmenafra@keyocoin.com>
2019-08-21 11:02:50 -0400

    new: Paper trail chef book

---
+++ papertrail/metadata.rb
@@ -0,0 +1,10 @@
+maintainer       "Librato, Inc."
+maintainer_email "mike@librato.com"
+license          "Apache 2.0"
+description      "Installs/Configures Sys Logging to papertrailapp.com"
+version          "0.0.7"
+name             "papertrail"
+
+depends          "rsyslog"
+
+recipe "papertrail", "Installs/Configures logging to papertrailapp.com"
